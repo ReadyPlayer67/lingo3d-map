9cf822501d0e20f9b7970e5520bb073903911074
liuqi <735611642@qq>
2022-04-01 10:53:17 +0800

    添加左右移动，跑步动作

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import {World, Model, ThirdPersonCamera, Skybox, useKeyboard, useLoop} from "lingo3d-react";
 import * as Lingo3d from 'lingo3d'
-import {useRef} from "react";
+import {useEffect, useRef} from "react";
 
 function App() {
     const key = useKeyboard()
     const characterRef = useRef<Lingo3d.Model>(null)
-    const movtion = key === 'w' ? 'walking' : 'idle'
+    const movtion = ['w','a','s','d','Shift w','w Shift'].includes(key) ? 'walking' : 'idle'
+    useEffect(() => {
+        console.log(key)
+    },[key])
     useLoop(() => {
         characterRef.current!.moveForward(-3)
     },key === 'w')
+    useLoop(() => {
+        characterRef.current!.moveForward(3)
+    },key === 's')
+    useLoop(() => {
+        characterRef.current!.moveRight(-3)
+    },key === 'd')
+    useLoop(() => {
+        characterRef.current!.moveRight(3)
+    },key === 'a')
+    useLoop(() => {
+        characterRef.current!.moveForward(-6)
+    },key.indexOf('w') > -1 && key.indexOf('Shift') > -1)
     return (
         <World>
             <Model src="Grassland.glb" scale={270} physics="map"/>
